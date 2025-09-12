28fc30959968021f5071b242b40e1aee439bb487
Noumnim <noumnim.tk225@gmail.com>
2025-09-12 17:03:28 +0700

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail image='https://d2zp5xs5cp8zlg.cloudfront.net/image-79322-800.jpg' title="A First Meetup" address='Some address 5, 12345 Some City' description='This is a first meetup' />
+    )
+}
+
+export default MeetupDetails    
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import MeetupList from './../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS=[
+    {
+        id:'m1',
+        title:'A First Meetup',
+        image:'https://d2zp5xs5cp8zlg.cloudfront.net/image-79322-800.jpg',
+        address:'Some address 5, 12345 Some City',
+        description: 'This is a first meetup'
+    },
+    {
+        id:'m2',
+        title:'A Second Meetup',
+        image:'https://d2zp5xs5cp8zlg.cloudfront.net/image-79322-800.jpg',
+        address:'Some address 10, 12345 Some City',
+        description: 'This is a second meetup'
+    }
+]
+function HomePage(props) {
+    return(
+        
+            <MeetupList meetups={props.meetups}/>  
+        
+    )
+}
+export async function getStaticProps(){
+return{
+    props:{
+        meetups:DUMMY_MEETUPS
+    }
+};
+}
+export default HomePage;
